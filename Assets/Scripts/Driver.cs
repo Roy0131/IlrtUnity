bd1309b84109ce2e2b446052320cb0551f9c7674
Roy <1150793678@qq.com>
2018-09-04 00:36:47 +0800

    增加空项目

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour 
+{
+    private void Awake()
+    {
+        Debug.Log("Driver!!!");
+        DontDestroyOnLoad(gameObject);
+    }
+}
